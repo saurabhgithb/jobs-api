23c93ae81e33d83aefd8b9dc9fbe47a4f1e0b84e
Saurabh <saurav999bsr@gmail.com>
2022-05-07 11:34:51 +0530

    added swagger docs

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// Security packages
 const helmet = require('helmet')
 const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimit = require('express-rate-limit')
+
+//Swagger UI
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
 
 //connectDB
 const connectDB = require('./db/connect')
@@ -32,8 +38,9 @@ app.use(cors())
 app.use(xss())
 
 app.get('/', (req, res)=>{
-  res.send('jobs api')
+  res.send('<h1>Jobs API</h1><a href = "/api-docs">Documentation</a>')
 })
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // routes
 app.use('/api/v1/auth', authRouter)
